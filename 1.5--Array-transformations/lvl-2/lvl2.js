c19524ef097147c798e98a986e9752f007270c1f
Edison <ronquillo5@hotmail.com>
2025-05-08 14:03:34 +0200

    all exercises from 1.6 done

--- 1.5--Array-transformations/lvl-2/lvl2.js
+++
@@ -1,6 +0,0 @@
-// Exercici 5
-
-const arr = [1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9];
-
-const final = arr.filter((x) => x >= 10).map(x => x * 2).reduce((sum, x) => sum + x);
-console.log(final);
--- 1.5--Array-transformations/lvl-3/lvl3.js
+++
@@ -1,8 +0,0 @@
-// Exercici 6
-
-const arr = [11, 12, 13, 14];
-
-const moreThanTen = (x) => x > 10;
-
-console.log("Every: " + arr.every(moreThanTen));
-console.log("Some: " + arr.some(moreThanTen));
